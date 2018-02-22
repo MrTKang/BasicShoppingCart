88dd452f88ff096f6c2de572fa2c66b18150b48f
Kevin Kang <taehyunkang@Taehyuns-Air.hitronhub.home>
2018-02-22 15:17:30 -0800

    sign up login templates

---
+++ login.php
@@ -0,0 +1,31 @@
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <title>Login</title>
+        <link href="css/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body >
+        <div class="login-container">
+            <form class="login-form" method="post" action="login.php">
+                <h1></h1>
+                <label>Email</label>
+                <input type="email" name="email" class="form-control" required="" autofocus="">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" required="">
+                <div class="checkbox mb-3">
+                    <label>
+                        <input type="checkbox" value="remember-me"> Remember me
+                    </label>
+                </div>
+                <button class="btn btn-lg btn-primary btn-block" name="submit" >Sign in</button>
+                <a href="signup">Sign Up</button>
+                <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
+            </form>
+        </div>
+    </body>
+</html>
---
+++ signup.php
@@ -0,0 +1,29 @@
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Sign Up</title>
+        <link href="css/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="signup-container">
+            <form class="signup-form" method="post" action="signup.php">
+                <h1 class="h3 mb-3 font-weight-normal">Please sign up</h1>
+                <h6></h6>
+                <label for="name">Your Name</label>
+                <input type="text" class="form-control" name="name" required="" autofocus="">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" name="email" required="" autofocus="">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password" required="">
+                <label for="passwordagain">Password Again</label>
+                <input type="password" class="form-control" name="passwordagain" required="">
+                <button id="sign-up-btn" class="btn btn-lg btn-primary btn-block"  name="submit" type="submit">Sign up</button>
+                <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
+            </form>
+        </div>
+    </body>
+</html>
